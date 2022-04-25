95501375fbaa33fbf4af15e7a1714fcfee2a797e
Yuri Makhov <yu.makhov@gmail.com>
2022-04-25 22:35:15 +0300

    handle redis exception

--- NetCore3WithReact.DAL/Repositories/GenericRepositoryWithCache.cs
+++ NetCore3WithReact.DAL/Repositories/GenericRepositoryWithCache.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Caching.Distributed;
 using NetCore3WithReact.DAL.Models;
 using NetCore3WithReact.Utilities;
+using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
@@ -38,16 +39,57 @@ namespace NetCore3WithReact.DAL.Repositories
 
         public T GetById(Guid id)
         {
-            var cacheKey = CreateItemCacheKey(id);
-            var cachedItemJson = _distributedCache.GetString(cacheKey);
-            if (cachedItemJson != null)
+            var cachedItem = GetCachedItem<T>(id);
+            if (cachedItem != null)
             {
-                return JsonSerializer.Deserialize<T>(cachedItemJson);
+                return cachedItem;
             }
 
             var entity = _decoratedRepository.GetById(id);
-            _distributedCache.SetString(cacheKey, JsonSerializer.Serialize(entity));
+            SetItemToCache(entity);
             return entity;
+        }
+
+        private TEntity GetCachedItem<TEntity>(Guid id) where TEntity : class, IIdentityModel
+        {
+            try
+            {
+                var cacheKey = CreateItemCacheKey(id);
+                var cachedItemJson = _distributedCache.GetString(cacheKey);
+                if (cachedItemJson == null)
+                {
+                    return null;
+                }
+
+                return JsonSerializer.Deserialize<TEntity>(cachedItemJson);
+            }
+            catch (RedisConnectionException)
+            {
+                //todo: would be good to use circuit breaker pattern here
+                return null;
+            }
+            catch (Exception)
+            {
+                //todo: log error
+                return null;
+            }
+        }
+
+        private void SetItemToCache<TEntity>(TEntity entity) where TEntity : class, IIdentityModel
+        {
+            try
+            {
+                var cacheKey = CreateItemCacheKey(entity.Id);
+                _distributedCache.SetString(cacheKey, JsonSerializer.Serialize(entity));
+            }
+            catch (RedisConnectionException)
+            {
+                //todo: would be good to use circuit breaker pattern here
+            }
+            catch (Exception)
+            {
+                //todo: log error
+            }
         }
 
         public void Insert(T entityToInsert)
@@ -62,9 +104,20 @@ namespace NetCore3WithReact.DAL.Repositories
         }
 
         private void InvalidateItemCache(Guid id)
-        {
-            var cacheKey = CreateItemCacheKey(id);
-            _distributedCache.Remove(cacheKey);
+        {            
+            try
+            {
+                var cacheKey = CreateItemCacheKey(id);
+                _distributedCache.Remove(cacheKey);
+            }
+            catch (RedisConnectionException)
+            {
+                //todo: would be good to use circuit breaker pattern here
+            }
+            catch (Exception)
+            {
+                //todo: log error
+            }
         }
 
         private string CreateItemCacheKey(Guid id)
